42b754298579feb45e494953d5fab2cea1a7b885
vahagn <vahagn258@gmail.com>
2024-10-24 21:13:49 +0400

    ongoing

--- suha-nextjs/next.config.mjs
+++ suha-nextjs/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/frontend",
+    // basePath: "/frontend",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
