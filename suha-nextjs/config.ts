dc9db965bccb6545cd66c147c57ba10e178ec393
Vahagn Tsarukyan <vahagn.tsarukyan@idt.am>
2024-10-25 13:01:11 +0400

    ongoing

---
+++ suha-nextjs/config.ts
@@ -0,0 +1,1 @@
+export const basePath = '/frontend/suha-nextjs/out';
--- suha-nextjs/src/data/blog_data.ts
+++ suha-nextjs/src/data/blog_data.ts
@@ -1,3 +1,5 @@
+import {basePath} from "../../config";
+
 interface DataType {
 	id: number;
 	img: string;
@@ -11,7 +13,7 @@ interface DataType {
 const blog_data: DataType[] = [
 	{
 		id: 1,
-		img: "/assets/img/bg-img/18.jpg",
+		img: `${basePath}/assets/img/bg-img/18.jpg`,
 		catagory: "Review",
 		title: "The 5 best reviews in Suha",
 		user_name: "Yasin",
@@ -20,7 +22,7 @@ const blog_data: DataType[] = [
 	},
 	{
 		id: 2,
-		img: "/assets/img/bg-img/19.jpg",
+		img: `${basePath}/assets/img/bg-img/19.jpg`,
 		catagory: "Shopping",
 		title: "The best deals of this week",
 		user_name: "Admin",
@@ -29,7 +31,7 @@ const blog_data: DataType[] = [
 	},
 	{
 		id: 3,
-		img: "/assets/img/bg-img/20.jpg",
+		img: `${basePath}/assets/img/bg-img/20.jpg`,
 		catagory: "Tips",
 		title: "5 tips for buy original products",
 		user_name: "Niloy",
@@ -38,7 +40,7 @@ const blog_data: DataType[] = [
 	},
 	{
 		id: 4,
-		img: "/assets/img/bg-img/21.jpg",
+		img: `${basePath}/assets/img/bg-img/21.jpg`,
 		catagory: "Offer",
 		title: "Mega Deals: Up to 75% discount",
 		user_name: "Dolly",
@@ -47,7 +49,7 @@ const blog_data: DataType[] = [
 	},
 	{
 		id: 5,
-		img: "/assets/img/bg-img/22.jpg",
+		img: `${basePath}/assets/img/bg-img/22.jpg`,
 		catagory: "Trends",
 		title: "Bridal shopping is the latest trends of this month",
 		user_name: "Sarah",
@@ -56,7 +58,7 @@ const blog_data: DataType[] = [
 	},
 	{
 		id: 6,
-		img: "/assets/img/bg-img/23.jpg",
+		img: `${basePath}/assets/img/bg-img/23.jpg`,
 		catagory: "News",
 		title: "How to easily buy a product",
 		user_name: "Suha",
