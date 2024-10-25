af448b7717a89c04d35a297183bbf27bc8453258
Vahagn Tsarukyan <vahagn.tsarukyan@idt.am>
2024-10-25 12:42:59 +0400

    ongoing

--- suha-nextjs/next.config.mjs
+++ suha-nextjs/next.config.mjs
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProd ? '/frontend/suha-nextjs/out' : '',
-    basePath: isProd ? '/frontend/suha-nextjs/out' : '',
+    assetPrefix: '/frontend/suha-nextjs/out',
+    basePath: '/frontend/suha-nextjs/out',
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
